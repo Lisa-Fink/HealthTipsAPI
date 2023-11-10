2de45d1092f7e42372e1b349200e77a0ec466a65
lisa fink <lisadfink@gmail.com>
2023-11-10 13:38:07 -0500

    Add / page

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from responses import *
 
 app = Flask(__name__)
 
+@app.route("/")
+def main():
+    return render_template("index.html")
 
 @app.route("/mood/<string:mood>")
 def mood(mood):
