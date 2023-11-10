441822229072cf0a3314aeb8d667711eb01ec89e
lisa fink <lisadfink@gmail.com>
2023-11-10 10:46:51 -0500

    Update routes/responses to correspond with supplied data

--- app.py
+++ app.py
@@ -1,59 +1,54 @@
 from flask import Flask
-from responses import (mood_responses, temperature_responses,
-                       heart_rate_responses, sleep_time_responses,
-                       season_responses)
+from responses import *
 
 app = Flask(__name__)
 
 
 @app.route("/mood/<string:mood>")
 def mood(mood):
-    if mood in mood_responses:
-        return {"health_tip": mood_responses[mood]}
+    if mood == "stormy" or mood == "anxiety":
+        return {"health_tip": mood_responses["stormy"]}
     else:
         # the mood is invalid return 404 status
         return {"error": "Mood not found"}, 404
 
-@app.route("/temperature/<float(1):temperature>")
-def temperature(temperature):
-    good = 97.4, 99.4
-    # temperature too low
-    if temperature < good[0]:
-        return {"health_tip": temperature_responses["low"]}
-    # temperature too high
-    if temperature > good[1]:
-        return {"health_tip": temperature_responses["high"]}
-    # temperature is good
-    return {"health_tip": temperature_responses["good"]}
-
-@app.route("/heart_rate/<int:heart_rate>")
-def heart_rate(heart_rate):
-    good = 60, 100
-    # heart rate too low
-    if heart_rate < good[0]:
-        return {"health_tip": heart_rate_responses["low"]}
-    # heart rate too high
-    if heart_rate > good[1]:
-        return {"health_tip": heart_rate_responses["high"]}
-    # heart rate is good
-    return {"health_tip": heart_rate_responses["good"]}
-
-@app.route("/sleep/<int:sleep_time>")
-def sleep(sleep_time):
-    good = 7, 8
-    # sleep time too low
-    if sleep_time < good[0]:
-        return {"health_tip": sleep_time_responses["low"]}
-    # sleep time too high
-    if sleep_time > good[1]:
-        return {"health_tip": sleep_time_responses["high"]}
-    # sleep time is good
-    return {"health_tip": sleep_time_responses["good"]}
-
-@app.route("/season/<string:season>")
-def season(season):
-    if season in season_responses:
-        return {"health_tip": season_responses[season]}
+@app.route("/temperature/<string:change>")
+def temperature(change):
+    if change == "increase" or change == "decrease":
+        return {"health_tip": temperature_responses[change]}
     else:
-        # the season is invalid return appropriate status
-        return {"error": "Season not found"}, 404
+        # the temperature change is invalid return 404 status
+        return {"error": "Temperature change not found"}, 404
+    
+@app.route("/working-out-time/<string:change>")
+def working_out_time(change):
+    if change == "increase":
+        return {"health_tip": working_out_time_responses[change]}
+    else:
+        # the working out time change is invalid return 404 status
+        return {"error": "Working out time change not found"}, 404
+    
+@app.route("/pysical-activity/<string:change>")
+def physical_activity(change):
+    if change == "decrease":
+        return {"health_tip": physical_activity_responses[change]}
+    else:
+        # the physical activity change is invalid return 404 status
+        return {"error": "Physical activity change not found"}, 404
+
+@app.route("/heart-rate/<string:change>")
+def heart_rate(change):
+    if change == "improve" or change == "decrease":
+        return {"health_tip": heart_rate_responses[change]}
+    else:
+        # the heart rate change is invalid return 404 status
+        return {"error": "Heart rate change not found"}, 404
+
+@app.route("/sleep-time/<string:change>")
+def sleep(change):
+    if change == "increase" or change == "reduce":
+        return {"health_tip": sleep_time_responses[change]}
+    else:
+        # the sleep change is invalid return 404 status
+        return {"error": "Sleep change not found"}, 404
+
--- responses.py
+++ responses.py
@@ -1,32 +1,136 @@
 mood_responses = {
-    "sunny": "sunny response",
-    "stormy": "stormy response",
-    "normal": "normal response",
-    "anxious": "anxious response",
-    "energetic": "energetic response"
+    "stormy":
+"""Create a plan — A well-prepared plan for your family can help reduce anxiety and
+chaos before, during, and afterward. Make an evacuation plan and compile
+preparedness kits.
+Be informed — Stay up-to-date on weather information and warnings. If you’re
+aware of the latest information, you may gain a sense of control over the situation.
+Talk it out — Share your fears with family members, friends, a counselor, or others
+who can offer emotional support.
+Accept what you can’t control — Nobody can control the path of a storm or its
+damage. And excessive worrying that one may hit you will not change anything
+except your emotional well-being.""",
+    "anxiety":
+"""Tips for anxiety mood.
+Here are 11 tips for coping with an anxiety disorder:
+Keep physically active.
+Develop a routine so that you're physically active most days of the week.
+Use stress management and relaxation techniques.
+Visualization techniques, meditation and yoga are examples of relaxation techniques that can
+ease anxiety.
+Make sleep a priority.
+Eat healthy foods.
+A healthy diet that incorporates vegetables, fruits, whole grains and fish may be linked to
+reduced anxiety, but more research is needed.
+Talk to your health care provider to find out what might be causing your specific condition and
+what treatments might be best for you. Involve your family and friends, and ask for their support.
+Stick to your treatment plan.
+Identify triggers.
+Learn what situations or actions cause you stress or increase your anxiety. Practice the
+strategies you developed with your mental health provider so you're ready to deal with anxious
+feelings in these situations.
+Keep a journal.
+Don't let worries isolate you from loved ones or activities.
+Your worries may not go away on their own but you can do something to stop them."""
+}
+
+working_out_time_responses = {
+    "increase":
+"""How to increase working out time in a week
+    ● Give yourself plenty of time to warm up and cool down with easy walking or
+    gentle stretching.
+    ● Speed up to a pace you can continue for five to 10 minutes without getting
+    overly tired.
+    ● Work your way up to 30 to 60 minutes of exercise most days of the week.
+    ● Grab the leash and walk your dog.
+    ● Take your kid (or your spouse) for a walk.
+    ● Try the 10-Minute workout."""
+}
+
+physical_activity_responses = {
+    "decrease":
+"""Incorporate Rest Days
+    ● Rest days allow your body to recover and reduce the risk of overtraining and burnout.
+      Modify Workout Intensity:
+      Consider incorporating periods of lighter training, such as active recovery days or deload
+      weeks.
+Diversify Your Activities:
+    ● Include activities that are less physically demanding, such as yoga, meditation, or
+      stretching.
+  Shorter Workout Sessions:
+    ● Shorten the duration of your workouts to allow more time for recovery.
+    ● Focus on quality rather than quantity during each session.
+  Adjust Frequency:
+    ● Reduce the frequency of intense workouts per week.
+  Listen to Your Body:
+    ● Adjust your workout intensity or take additional rest days if needed.
+  Prioritize Sleep:
+    ● Ensure you are getting adequate and quality sleep, as it plays a crucial role in
+    recovery.
+    ● Lack of sleep can contribute to increased stress on the body.
+  Manage Stress:
+    ● Incorporate stress-reducing activities into your routine, such as mindfulness,
+    deep breathing, or hobbies.
+    ● Chronic stress can negatively impact recovery and overall well-being."""
 }
 
 temperature_responses = {
-    "high": "too high",
-    "good": "good",
-    "low": "too low"
+    "increase":
+"""Remove wet clothes
+Get out of the cold
+Rely on skin to skin contact
+Warm the center of the body first
+Keep warm in cold weather
+Put on additional clothing
+Consume hot foods and warm liquids
+How to decrease temperature
+Keep moving""",
+    "decrease":
+"""Drink cool liquids like water or iced tea to cool the body internally
+Go somewhere with cooler air or get in cool water to reduce heat loss from the skin
+Apply cold to key points on the body, such as the wrists, neck, or temples.
+Move less and wear lighter, more breathable clothing to avoid generating excess heat
+Take heat regulating supplements, such as peppermint, ginger, or turmeric
+Talk to a doctor about thyroid health, as thyroid disorders can affect body temperature.
+Exercise in less physically demanding ways or swim in cool water to prevent
+overheating.
+Do controlled breathing exercises to lower your heart rate and core temperature."""
 }
 
 heart_rate_responses = {
-    "high": "too high",
-    "good": "good",
-    "low": "too low"
+    "improve":
+"""● Set an incline. If you’re on the treadmill increase the incline. Or if you’re walking outside look
+for hills. This will challenge your muscles and help increase your heart rate.
+● Take the stairs. Just like adding an incline, stairs bring a new challenge to your workout.
+● Alter your pace. Whether you’re walking, riding a bike, swimming or practicing yoga, you
+don’t have to increase your pace for the entire workout. Add in short bursts of increased
+effort at a faster pace. Over time, you’ll be able to increase the duration of these bursts.
+● Take shorter breaks. If you’re doing an interval workout or lifting weights, take shorter breaks
+in between the different exercises.""",
+    "decrease":
+"""Exercise
+Stay hydrated
+Limit taking of stimulants
+Limit alcohol intake
+Eat nutritious
+Have a balanced diet
+Get enough sleep."""
 }
 
-sleep_time_responses =  {
-    "high": "too high",
-    "good": "good",
-    "low": "too low"
+sleep_time_responses = {
+    "increase":
+"""Avoid caffeine, alcohol, nicotine, and other chemicals that interfere with sleep,
+especially in the afternoon and evening.
+Turn your bedroom into a sleep-inducing environment, with room-darkening shades,
+earplugs, a fan, or other devices that suit your needs.
+Establish a soothing pre-sleep routine, such as taking a warm bath, doing calming
+activities, or listening to binaural beats.
+Go to sleep when you’re truly tired and maintain a set sleep schedule.
+Dedicate time to self-care, such as yoga, meditation, journaling, reading, or listening to
+relaxing music.""",
+    "reduce":
+"""Check in with your consultant at least once a week
+Manage stress
+Do sleep routines"""
 }
 
-season_responses = {
-    "spring": "spring response",
-    "summer": "summer response",
-    "fall": "fall response",
-    "winter": "winter response",
-}
